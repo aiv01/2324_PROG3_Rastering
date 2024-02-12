d44f14445ab2d145a560d11a236aa8824f2f2582
zabuza-momochi <104208249+zabuza-momochi@users.noreply.github.com>
2024-02-12 23:41:43 +0100

    Update obj ctor

--- appl/include/obj_parser.h
+++ appl/include/obj_parser.h
@@ -25,7 +25,7 @@ typedef struct obj_float2_t
     float y;
 } obj_float2_t;
 
-typedef struct obj_t
+typedef struct obj_parse_t
 {
     int v_count;
     int vt_count;
@@ -35,17 +35,35 @@ typedef struct obj_t
     obj_float2_t *vt;
     obj_float3_t *vn;
     obj_vertex_info_t *v_info;
+} obj_parse_t;
 
+typedef struct
+{
+    obj_float3_t position; // alias 'v'
+    obj_float3_t normal;   // alias 'vn'
+    obj_float2_t uv;       // alias 'vt'
+} obj_vertex_t;
+
+typedef struct
+{
+    obj_vertex_t v1;
+    obj_vertex_t v2;
+    obj_vertex_t v3;
+} obj_triangle_t;
+
+typedef struct
+{
+    obj_triangle_t *triangles;
+    size_t triangle_count;
 } obj_t;
 
-obj_t *obj_parse(const char *file_name)
+obj_parse_t *obj_parser(const char *file_name)
 {
-
-    obj_t *obj = (obj_t *)malloc(sizeof(obj_t));
-    obj->v_count = 0;
-    obj->vt_count = 0;
-    obj->vn_count = 0;
-    obj->f_count = 0;
+    obj_parse_t *obj_parse = (obj_parse_t *)malloc(sizeof(obj_parse_t));
+    obj_parse->v_count = 0;
+    obj_parse->vt_count = 0;
+    obj_parse->vn_count = 0;
+    obj_parse->f_count = 0;
 
     FILE *file;
     fopen_s(&file, file_name, "r");
@@ -62,29 +80,29 @@ obj_t *obj_parse(const char *file_name)
     {
         if (strncmp(buffer, "v ", 2) == 0)
         {
-            obj->v_count++;
+            obj_parse->v_count++;
         }
 
         if (strncmp(buffer, "vt ", 3) == 0)
         {
-            obj->vt_count++;
+            obj_parse->vt_count++;
         }
 
         if (strncmp(buffer, "vn ", 3) == 0)
         {
-            obj->vn_count++;
+            obj_parse->vn_count++;
         }
 
         if (strncmp(buffer, "f ", 2) == 0)
         {
-            obj->f_count++;
+            obj_parse->f_count++;
         }
     }
 
-    obj->v = (obj_float3_t *)malloc(obj->v_count * sizeof(obj_float3_t));
-    obj->vt = (obj_float2_t *)malloc(obj->vt_count * sizeof(obj_float2_t));
-    obj->vn = (obj_float3_t *)malloc(obj->vn_count * sizeof(obj_float3_t));
-    obj->v_info = (obj_vertex_info_t *)malloc(obj->f_count * sizeof(obj_vertex_info_t) * 3);
+    obj_parse->v = (obj_float3_t *)malloc(obj_parse->v_count * sizeof(obj_float3_t));
+    obj_parse->vt = (obj_float2_t *)malloc(obj_parse->vt_count * sizeof(obj_float2_t));
+    obj_parse->vn = (obj_float3_t *)malloc(obj_parse->vn_count * sizeof(obj_float3_t));
+    obj_parse->v_info = (obj_vertex_info_t *)malloc(obj_parse->f_count * sizeof(obj_vertex_info_t) * 3);
 
     rewind(file); // seeks file at start
 
@@ -103,13 +121,13 @@ obj_t *obj_parse(const char *file_name)
             strtok_s(buffer, " ", &remaining_tokens); // v token
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->v[v_index].x = atof(token);
+            obj_parse->v[v_index].x = atof(token);
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->v[v_index].y = atof(token);
+            obj_parse->v[v_index].y = atof(token);
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->v[v_index].z = atof(token);
+            obj_parse->v[v_index].z = atof(token);
 
             v_index++;
         }
@@ -122,10 +140,10 @@ obj_t *obj_parse(const char *file_name)
             strtok_s(buffer, " ", &remaining_tokens); // vt token
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->vt[vt_index].x = atof(token);
+            obj_parse->vt[vt_index].x = atof(token);
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->vt[vt_index].y = atof(token);
+            obj_parse->vt[vt_index].y = atof(token);
 
             vt_index++;
         }
@@ -138,13 +156,13 @@ obj_t *obj_parse(const char *file_name)
             strtok_s(buffer, " ", &remaining_tokens); // vt token
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->vn[vn_index].x = atof(token);
+            obj_parse->vn[vn_index].x = atof(token);
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->vn[vn_index].y = atof(token);
+            obj_parse->vn[vn_index].y = atof(token);
 
             token = strtok_s(NULL, " ", &remaining_tokens);
-            obj->vn[vn_index].z = atof(token);
+            obj_parse->vn[vn_index].z = atof(token);
 
             vn_index++;
         }
@@ -160,13 +178,13 @@ obj_t *obj_parse(const char *file_name)
             {
                 token = strtok_s(NULL, "/", &remaining_tokens);
 
-                obj->v_info[f_index + i].v_index = atoi(token);
+                obj_parse->v_info[f_index + i].v_index = atoi(token);
 
                 token = strtok_s(NULL, "/", &remaining_tokens);
-                obj->v_info[f_index + i].vt_index = atoi(token);
+                obj_parse->v_info[f_index + i].vt_index = atoi(token);
 
                 token = strtok_s(NULL, " ", &remaining_tokens);
-                obj->v_info[f_index + i].vn_index = atoi(token);
+                obj_parse->v_info[f_index + i].vn_index = atoi(token);
             }
 
             f_index += 3;
@@ -175,7 +193,103 @@ obj_t *obj_parse(const char *file_name)
 
     fclose(file);
 
+    return obj_parse;
+}
+
+obj_t *obj_ctor(obj_parse_t *obj_parse)
+{
+    // Allocata obj struct
+    obj_t *obj = (obj_t *)malloc(sizeof(obj_t));
+
+    // Init triangle count
+    obj->triangle_count = obj_parse->f_count;
+
+    // Allocate triangles struct
+    obj->triangles = (obj_triangle_t *)malloc(sizeof(obj_triangle_t) * obj->triangle_count);
+
+    // Loop faces
+    for (size_t i = 0; i < obj->triangle_count; i++)
+    {
+        // Set current face index for v_info
+        int f_index = i * 3;
+
+        // Set indexes for vertex 1
+        int index_v1 = obj_parse->v_info[f_index + 0].v_index - 1;
+        int index_vt1 = obj_parse->v_info[f_index + 0].vt_index - 1;
+        int index_vn1 = obj_parse->v_info[f_index + 0].vn_index - 1;
+
+        // Set position info for vertex 1
+        obj->triangles[i].v1.position.x = obj_parse->v[index_v1].x;
+        obj->triangles[i].v1.position.y = obj_parse->v[index_v1].y;
+        obj->triangles[i].v1.position.z = obj_parse->v[index_v1].z;
+
+        // Set uv info for vertex 1
+        obj->triangles[i].v1.uv.x = obj_parse->vt[index_vt1].x;
+        obj->triangles[i].v1.uv.y = obj_parse->vt[index_vt1].y;
+
+        // Set normals info for vertex 1
+        obj->triangles[i].v1.normal.x = obj_parse->vn[index_vn1].x;
+        obj->triangles[i].v1.normal.y = obj_parse->vn[index_vn1].y;
+        obj->triangles[i].v1.normal.z = obj_parse->vn[index_vn1].z;
+
+        // Set indexes for vertex 2
+        int index_v2 = obj_parse->v_info[f_index + 1].v_index - 1;
+        int index_vt2 = obj_parse->v_info[f_index + 1].vt_index - 1;
+        int index_vn2 = obj_parse->v_info[f_index + 1].vn_index - 1;
+
+        // Set position info for vertex 2
+        obj->triangles[i].v2.position.x = obj_parse->v[index_v2].x;
+        obj->triangles[i].v2.position.y = obj_parse->v[index_v2].y;
+        obj->triangles[i].v2.position.z = obj_parse->v[index_v2].z;
+        
+        // Set uv info for vertex 2
+        obj->triangles[i].v2.uv.x = obj_parse->vt[index_vt2].x;
+        obj->triangles[i].v2.uv.y = obj_parse->vt[index_vt2].y;
+
+        // Set normals info for vertex 2
+        obj->triangles[i].v2.normal.x = obj_parse->vn[index_vn2].x;
+        obj->triangles[i].v2.normal.y = obj_parse->vn[index_vn2].y;
+        obj->triangles[i].v2.normal.z = obj_parse->vn[index_vn2].z;
+
+        // Set indexes for vertex 3
+        int index_v3 = obj_parse->v_info[f_index + 2].v_index - 1;
+        int index_vt3 = obj_parse->v_info[f_index + 2].vt_index - 1;
+        int index_vn3 = obj_parse->v_info[f_index + 2].vn_index - 1;
+
+        // Set position info for vertex 3
+        obj->triangles[i].v3.position.x = obj_parse->v[index_v3].x;
+        obj->triangles[i].v3.position.y = obj_parse->v[index_v3].y;
+        obj->triangles[i].v3.position.z = obj_parse->v[index_v3].z;
+
+        // Set uv info for vertex 3
+        obj->triangles[i].v3.uv.x = obj_parse->vt[index_vt3].x;
+        obj->triangles[i].v3.uv.y = obj_parse->vt[index_vt3].y;
+
+        // Set normals info for vertex 3
+        obj->triangles[i].v3.normal.x = obj_parse->vn[index_vn3].x;
+        obj->triangles[i].v3.normal.y = obj_parse->vn[index_vn3].y;
+        obj->triangles[i].v3.normal.z = obj_parse->vn[index_vn3].z;
+    }
+
     return obj;
+}
+
+void obj_parser_destroy(obj_t *obj_ctor, obj_parse_t *obj_parse)
+{
+    if (obj_parse != NULL)
+    {
+        free(obj_parse->v);
+        free(obj_parse->vt);
+        free(obj_parse->vn);
+        free(obj_parse->v_info);
+        free(obj_parse);
+    }
+
+    if (obj_ctor != NULL)
+    {
+        free(obj_ctor->triangles);
+        free(obj_ctor);
+    }
 }
 
 #endif // end define OBJ_PARSER
--- test/src/obj_parser_test.c
+++ test/src/obj_parser_test.c
@@ -5,86 +5,180 @@
 CLOVE_TEST(ReadQuadObj)
 {
    char *file_name = "bin\\test\\resources\\quad.obj";
-   obj_t *obj = obj_parse(file_name);
+   obj_parse_t *obj_parse = obj_parser(file_name);
 
-   CLOVE_NOT_NULL(obj);
-   CLOVE_INT_EQ(4, obj->v_count);
-   CLOVE_INT_EQ(4, obj->vt_count);
-   CLOVE_INT_EQ(1, obj->vn_count);
-   CLOVE_INT_EQ(2, obj->f_count);
+   CLOVE_NOT_NULL(obj_parse);
+   CLOVE_INT_EQ(4, obj_parse->v_count);
+   CLOVE_INT_EQ(4, obj_parse->vt_count);
+   CLOVE_INT_EQ(1, obj_parse->vn_count);
+   CLOVE_INT_EQ(2, obj_parse->f_count);
 
    // vertex tests
-   CLOVE_FLOAT_EQ(-1.f, obj->v[0].x);
-   CLOVE_FLOAT_EQ(1.f, obj->v[0].y);
-   CLOVE_FLOAT_EQ(0.f, obj->v[0].z);
+   CLOVE_FLOAT_EQ(-1.f, obj_parse->v[0].x);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->v[0].y);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->v[0].z);
 
-   CLOVE_FLOAT_EQ(1.f, obj->v[1].x);
-   CLOVE_FLOAT_EQ(1.f, obj->v[1].y);
-   CLOVE_FLOAT_EQ(0.f, obj->v[1].z);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->v[1].x);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->v[1].y);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->v[1].z);
 
-   CLOVE_FLOAT_EQ(-1.f, obj->v[2].x);
-   CLOVE_FLOAT_EQ(-1.f, obj->v[2].y);
-   CLOVE_FLOAT_EQ(0.f, obj->v[2].z);
+   CLOVE_FLOAT_EQ(-1.f, obj_parse->v[2].x);
+   CLOVE_FLOAT_EQ(-1.f, obj_parse->v[2].y);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->v[2].z);
 
-   CLOVE_FLOAT_EQ(1.f, obj->v[3].x);
-   CLOVE_FLOAT_EQ(-1.f, obj->v[3].y);
-   CLOVE_FLOAT_EQ(0.f, obj->v[3].z);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->v[3].x);
+   CLOVE_FLOAT_EQ(-1.f, obj_parse->v[3].y);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->v[3].z);
 
    // uv tests
-   CLOVE_FLOAT_EQ(1.f, obj->vt[0].x);
-   CLOVE_FLOAT_EQ(1.f, obj->vt[0].y);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->vt[0].x);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->vt[0].y);
 
-   CLOVE_FLOAT_EQ(0.f, obj->vt[1].x);
-   CLOVE_FLOAT_EQ(1.f, obj->vt[1].y);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->vt[1].x);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->vt[1].y);
 
-   CLOVE_FLOAT_EQ(0.f, obj->vt[2].x);
-   CLOVE_FLOAT_EQ(0.f, obj->vt[2].y);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->vt[2].x);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->vt[2].y);
 
-   CLOVE_FLOAT_EQ(1.f, obj->vt[3].x);
-   CLOVE_FLOAT_EQ(0.f, obj->vt[3].y);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->vt[3].x);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->vt[3].y);
 
    // normals tests
-   CLOVE_FLOAT_EQ(0.f, obj->vn[0].x);
-   CLOVE_FLOAT_EQ(0.f, obj->vn[0].y);
-   CLOVE_FLOAT_EQ(1.f, obj->vn[0].z);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->vn[0].x);
+   CLOVE_FLOAT_EQ(0.f, obj_parse->vn[0].y);
+   CLOVE_FLOAT_EQ(1.f, obj_parse->vn[0].z);
 
-   // faces tests
+   // info test
+   CLOVE_NOT_NULL(obj_parse);
+   CLOVE_INT_EQ(4, obj_parse->v_count);
+   CLOVE_INT_EQ(4, obj_parse->vt_count);
+   CLOVE_INT_EQ(1, obj_parse->vn_count);
+   CLOVE_INT_EQ(2, obj_parse->f_count);
 
-   //face 1
-   CLOVE_INT_EQ(4, obj->v_info[0].v_index);
-   CLOVE_INT_EQ(4, obj->v_info[0].vt_index);
-   CLOVE_INT_EQ(1, obj->v_info[0].vn_index);
+   // faces test
 
-   CLOVE_INT_EQ(3, obj->v_info[1].v_index);
-   CLOVE_INT_EQ(3, obj->v_info[1].vt_index);
-   CLOVE_INT_EQ(1, obj->v_info[1].vn_index);
+   // face 1
+   CLOVE_INT_EQ(4, obj_parse->v_info[0].v_index);
+   CLOVE_INT_EQ(4, obj_parse->v_info[0].vt_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[0].vn_index);
 
-   CLOVE_INT_EQ(1, obj->v_info[2].v_index);
-   CLOVE_INT_EQ(2, obj->v_info[2].vt_index);
-   CLOVE_INT_EQ(1, obj->v_info[2].vn_index);
+   CLOVE_INT_EQ(3, obj_parse->v_info[1].v_index);
+   CLOVE_INT_EQ(3, obj_parse->v_info[1].vt_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[1].vn_index);
 
-   //face 2
-   CLOVE_INT_EQ(2, obj->v_info[3].v_index);
-   CLOVE_INT_EQ(1, obj->v_info[3].vt_index);
-   CLOVE_INT_EQ(1, obj->v_info[3].vn_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[2].v_index);
+   CLOVE_INT_EQ(2, obj_parse->v_info[2].vt_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[2].vn_index);
 
-   CLOVE_INT_EQ(4, obj->v_info[4].v_index);
-   CLOVE_INT_EQ(4, obj->v_info[4].vt_index);
-   CLOVE_INT_EQ(1, obj->v_info[4].vn_index);
+   // face 2
+   CLOVE_INT_EQ(2, obj_parse->v_info[3].v_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[3].vt_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[3].vn_index);
 
-   CLOVE_INT_EQ(1, obj->v_info[5].v_index);
-   CLOVE_INT_EQ(2, obj->v_info[5].vt_index);
-   CLOVE_INT_EQ(1, obj->v_info[5].vn_index);
+   CLOVE_INT_EQ(4, obj_parse->v_info[4].v_index);
+   CLOVE_INT_EQ(4, obj_parse->v_info[4].vt_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[4].vn_index);
+
+   CLOVE_INT_EQ(1, obj_parse->v_info[5].v_index);
+   CLOVE_INT_EQ(2, obj_parse->v_info[5].vt_index);
+   CLOVE_INT_EQ(1, obj_parse->v_info[5].vn_index);
 }
 
 CLOVE_TEST(CheckSuzanneCounts)
 {
    char *file_name = "bin\\test\\resources\\suzanne.obj";
-   obj_t *obj = obj_parse(file_name);
+   obj_parse_t *obj_parse = obj_parser(file_name);
 
-   CLOVE_NOT_NULL(obj);
-   CLOVE_INT_EQ(511, obj->v_count);
-   CLOVE_INT_EQ(590, obj->vt_count);
-   CLOVE_INT_EQ(507, obj->vn_count);
-   CLOVE_INT_EQ(968, obj->f_count);
+   CLOVE_NOT_NULL(obj_parse);
+   CLOVE_INT_EQ(511, obj_parse->v_count);
+   CLOVE_INT_EQ(590, obj_parse->vt_count);
+   CLOVE_INT_EQ(507, obj_parse->vn_count);
+   CLOVE_INT_EQ(968, obj_parse->f_count);
+}
+
+CLOVE_TEST(CheckObjCtor)
+{
+   char *file_name = "bin\\test\\resources\\quad.obj";
+
+   obj_parse_t *obj_parse = obj_parser(file_name);
+   CLOVE_NOT_NULL(obj_parse);
+
+   obj_t *obj_quad = obj_ctor(obj_parse);
+   CLOVE_NOT_NULL(obj_quad);
+
+   // Test: FACE 1
+   // Test: Vertex 1
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v1.position.x);
+   CLOVE_FLOAT_EQ(-1, obj_quad->triangles[0].v1.position.y);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v1.position.z);
+
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v1.uv.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v1.uv.y);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v1.normal.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v1.normal.y);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v1.normal.z);
+
+   // Test: Vertex 2
+   CLOVE_FLOAT_EQ(-1, obj_quad->triangles[0].v2.position.x);
+   CLOVE_FLOAT_EQ(-1, obj_quad->triangles[0].v2.position.y);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v2.position.z);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v2.uv.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v2.uv.y);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v2.normal.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v2.normal.y);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v2.normal.z);
+
+   // Test: Vertex 3
+   CLOVE_FLOAT_EQ(-1, obj_quad->triangles[0].v3.position.x);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v3.position.y);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v3.position.z);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v3.uv.x);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v3.uv.y);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v3.normal.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[0].v3.normal.y);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[0].v3.normal.z);
+
+   // Test FACE 2
+   // Test: Verte 1
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v1.position.x);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v1.position.y);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v1.position.z);
+
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v1.uv.x);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v1.uv.y);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v1.normal.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v1.normal.y);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v1.normal.z);
+
+   // Test: Vertex 2
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v2.position.x);
+   CLOVE_FLOAT_EQ(-1, obj_quad->triangles[1].v2.position.y);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v2.position.z);
+
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v2.uv.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v2.uv.y);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v2.normal.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v2.normal.y);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v2.normal.z);
+
+   // Test: Vertex 3
+   CLOVE_FLOAT_EQ(-1, obj_quad->triangles[1].v3.position.x);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v3.position.y);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v3.position.z);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v3.uv.x);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v3.uv.y);
+
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v3.normal.x);
+   CLOVE_FLOAT_EQ(0, obj_quad->triangles[1].v3.normal.y);
+   CLOVE_FLOAT_EQ(1, obj_quad->triangles[1].v3.normal.z);
+
+   obj_parser_destroy(obj_quad, obj_parse);
 }
